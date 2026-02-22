0d3fe183c82763f07f805f4b0c95acbd9ac06cf5
Ashik-Ahammad <ashik19ahammad@gmail.com>
2026-02-22 16:28:11 +0600

    Add Functionality

--- README.md
+++ README.md
@@ -1,6 +1,29 @@
-Answers to Questions:
+# 📘 JavaScript DOM Questions & Answers
+
 1. What is the difference between getElementById, getElementsByClassName, and querySelector / querySelectorAll?
 2. How do you create and insert a new element into the DOM?
 3. What is Event Bubbling? And how does it work?
 4. What is Event Delegation in JavaScript? Why is it useful?
 5. What is the difference between preventDefault() and stopPropagation() methods?
+
+## ✅ Answers
+
+```
+1. The difference between getElementById, getElementsByClassName, querySelector, and querySelectorAll is how they select elements. getElementById() selects only one element using its id. getElementsByClassName() selects all elements that have the same class name and returns a collection. querySelector() uses CSS selectors and returns only the first matching element. querySelectorAll() also uses CSS selectors but returns all matching elements as a list.
+```
+
+```
+2. To create and insert a new element into the DOM, first you create the element using document.createElement(). Then you add text or content inside it using properties like innerText or innerHTML. After that, you insert it into the page by using methods like appendChild() or append() on a parent element. This is how new elements are dynamically added to a webpage.
+```
+
+```
+3. Event bubbling is a process in JavaScript where an event starts from the target element and then moves upward to its parent elements. For example, if you click a button inside a div, the click event first happens on the button, then on the div, then on the body, and continues upward. This upward movement is called bubbling.
+```
+
+```
+4.  Event delegation is a technique where you add an event listener to a parent element instead of adding listeners to many child elements. The parent listens for events that happen on its children and handles them. This is useful because it reduces code, improves performance, and also works for new elements that are added later dynamically.
+```
+
+```
+5. The difference between preventDefault() and stopPropagation() is in what they stop. preventDefault() stops the browser’s default action, such as stopping a form from submitting or a link from opening. On the other hand, stopPropagation() stops the event from moving up to parent elements, which means it stops event bubbling but does not affect the default browser behavior.
+```
--- scripts/index.js
+++ scripts/index.js
@@ -11,6 +11,16 @@ const allCardSecion = document.getElementById("allCards");
 const interviewFilterSection = document.getElementById("interviewCards");
 const rejectedFilterSection = document.getElementById("rejectedCards");
 
+const emptyMessage = `
+<div class="flex flex-col justify-center items-center text-center my-15">
+  <div><img src="assets/jobs.png" alt=""></div>
+  <div>
+    <p class="text-xl text-blue-950 font-semibold">No Jobs Available</p>
+    <p class="text-gray-400">Check back soon for new job opportunities</p>
+  </div>
+</div>
+`;
+
 // Function to calculate the counts
 function calculateCount() {
 
@@ -29,6 +39,12 @@ function calculateCount() {
   }
 }
 calculateCount();
+
+const allFilterBtn = document.getElementById("all-filter-btn");
+const interviewFilterBtn = document.getElementById("interview-filter-btn");
+const rejectedFilterBtn = document.getElementById("rejected-filter-btn");
+
+const mainContainer = document.querySelector("main");
 
 // handling button clicks
 function toggleStyle(id) {
@@ -74,4 +90,235 @@ function toggleStyle(id) {
   }
 
   calculateCount();
+}
+
+mainContainer.addEventListener("click", (event) => {
+
+  const parentNode = event.target.parentNode.parentNode;
+  if(!parentNode) return;
+
+  const companyName = parentNode.querySelector(".companyName").innerText;
+  const designation = parentNode.querySelector(".designation").innerText;
+  const location = parentNode.querySelector(".job-location").innerText;
+  const workingHour = parentNode.querySelector(".job-hour").innerText;
+  const salary = parentNode.querySelector(".job-salary").innerText;
+  const notes = parentNode.querySelector(".notes").innerText;
+
+  // ================= Interview =================
+  if (event.target.classList.contains("interview-btn")) {
+
+    parentNode.querySelector(".my-status").innerText = "Interview";
+
+    rejectedList = rejectedList.filter(job => job.companyName !== companyName);
+
+    const exists = interviewList.find(job => job.companyName === companyName);
+
+    if(!exists){
+      interviewList.push({
+        companyName,
+        designation,
+        location,
+        workingHour,
+        salary,
+        status:"Interview",
+        notes
+      });
+    }
+
+    renderInterviewCards();
+    renderRejectedCards();
+    calculateCount();
+  }
+
+
+  // ================= Rejected =================
+  if (event.target.classList.contains("rejected-btn")) {
+
+    parentNode.querySelector(".my-status").innerText = "Rejected";
+
+    interviewList = interviewList.filter(job => job.companyName !== companyName);
+
+    const exists = rejectedList.find(job => job.companyName === companyName);
+
+    if(!exists){
+      rejectedList.push({
+        companyName,
+        designation,
+        location,
+        workingHour,
+        salary,
+        status:"Rejected",
+        notes
+      });
+    }
+
+    renderInterviewCards();
+    renderRejectedCards();
+    calculateCount();
+  }
+
+  // ================= Delete Button =================
+if (event.target.closest(".delete-btn")) {
+
+  const card = event.target.closest(".card");
+  const companyName = card.querySelector(".companyName").innerText;
+
+  // remove from interview list
+  interviewList = interviewList.filter(
+    job => job.companyName !== companyName
+  );
+
+  // remove from rejected list
+  rejectedList = rejectedList.filter(
+    job => job.companyName !== companyName
+  );
+
+  // remove from UI
+  card.remove();
+
+  // refresh sections instantly
+  renderInterviewCards();
+  renderRejectedCards();
+  calculateCount();
+}
+
+});
+
+function renderInterviewCards() {
+  interviewFilterSection.innerHTML = "";
+
+  if (interviewList.length === 0) {
+    interviewFilterSection.innerHTML = emptyMessage;
+    return;
+  }
+
+  for (let interview of interviewList) {
+    let div = document.createElement("div");
+    div.classList.add(
+      "relative",
+      "flex",
+      "flex-col",
+      "md:flex-row",
+      "md:justify-between",
+      "md:items-start",
+      "gap-4",
+    );
+    div.innerHTML = `
+        <div
+            class="card bg-base-100 shadow-lg border border-gray-200 p-5 rounded-2xl w-full mt-5"
+          >
+        <div class="space-y-4 w-full">
+              <div>
+                <h2 class="text-lg font-bold text-blue-950 companyName">
+                  ${interview.companyName}
+                </h2>
+                <p class="text-gray-500 designation">${interview.designation}</p>
+              </div>
+
+              <div class="flex flex-wrap gap-x-3 gap-y-1 text-sm">
+                <p class="text-gray-600 job-location">${interview.location}</p>
+                <p class="text-gray-600 job-hour">${interview.workingHour}</p>
+                <p class="text-gray-600 job-salary">$130,000 - $175,000</p>
+              </div>
+
+              <div class="flex items-center justify-between">
+                <div
+                  class="flex items-center gap-2 bg-blue-100 py-1 px-2 rounded w-fit"
+                >
+                  <p class="my-status text-sm">${interview.status}</p>
+                </div>
+              </div>
+
+              <div class="pe-4 py-3">
+                <p class="text-sm text-gray-600 notes">
+                    ${interview.notes}
+                </p>
+              </div>
+
+              <div class="flex flex-col sm:flex-row gap-3">
+                <button
+                  class="interview-btn btn btn-outline btn-success btn-sm"
+                >
+                  INTERVIEW
+                </button>
+                <button class="rejected-btn btn btn-outline btn-error btn-sm">
+                  REJECTED
+                </button>
+              </div>
+            </div>
+            <button
+              class="delete-btn absolute top-4 right-4 btn btn-outline btn-sm btn-circle text-red-500 hover:bg-red-500 hover:text-white"
+            >
+              <i class="fa-solid fa-trash-can"></i>
+            </button>
+        `;
+
+    interviewFilterSection.appendChild(div);
+  }
+}
+
+function renderRejectedCards(){
+
+    rejectedFilterSection.innerHTML= "";
+
+    if (rejectedList.length === 0) {
+    rejectedFilterSection.innerHTML = emptyMessage;
+    return;
+  }
+
+    for(let rejected of rejectedList){
+
+        let div = document.createElement("div");
+        div.classList.add("relative", "flex", "flex-col", "md:flex-row", "md:justify-between", "md:items-start", "gap-4");
+
+        div.innerHTML = `
+        <div
+            class="card bg-base-100 shadow-lg border border-gray-200 p-5 rounded-2xl w-full mt-5"
+        >
+            <div class="space-y-4 w-full">
+
+                <div>
+                    <h2 class="text-lg font-bold text-blue-950 companyName">
+                        ${rejected.companyName}
+                    </h2>
+                    <p class="text-gray-500 designation">${rejected.designation}</p>
+                </div>
+
+                <div class="flex flex-wrap gap-x-3 gap-y-1 text-sm">
+                    <p class="text-gray-600 job-location">${rejected.location}</p>
+                    <p class="text-gray-600 job-hour">${rejected.workingHour}</p>
+                    <p class="text-gray-600 job-salary">${rejected.salary}</p>
+                </div>
+
+                <div class="flex items-center justify-between">
+                    <div class="flex items-center gap-2 bg-red-100 py-1 px-2 rounded w-fit">
+                        <p class="my-status text-sm">${rejected.status}</p>
+                    </div>
+                </div>
+
+                <div class="pe-4 py-3">
+                    <p class="text-sm text-gray-600 notes">
+                        ${rejected.notes}
+                    </p>
+                </div>
+
+                <div class="flex flex-col sm:flex-row gap-3">
+                    <button class="interview-btn btn btn-outline btn-success btn-sm">
+                        INTERVIEW
+                    </button>
+                    <button class="rejected-btn btn btn-outline btn-error btn-sm">
+                        REJECTED
+                    </button>
+                </div>
+            </div>
+            <button
+              class="delete-btn absolute top-4 right-4 btn btn-outline btn-sm btn-circle text-red-500 hover:bg-red-500 hover:text-white"
+            >
+              <i class="fa-solid fa-trash-can"></i>
+            </button>
+        </div>
+        `
+
+        rejectedFilterSection.appendChild(div);
+    }
 }
